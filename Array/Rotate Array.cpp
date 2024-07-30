826cd9d6525a5676683751e2f4872f979c48c4ad
ash9041 <121339530+ash9041@users.noreply.github.com>
2024-07-30 18:34:39 +0530

    problem done

---
+++ Array/Rotate Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+      int n=nums.size();
+      k=k%nums.size();
+      int i=0;
+      int j=n-k-1;
+      while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+
+      }
+      i=n-k;
+      j=n-1;
+      while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+      }
+      i=0;
+      j=n-1;
+      while(i<j){
+        swap(nums[i],nums[j]);
+        i++;
+        j--;
+      }
+    }
+};
